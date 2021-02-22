830bc4dcd0a986a78db89324b035bde15dc9ba77
Jake Elizondo <jake.elizondo23@gmail.com>
2021-02-22 16:28:33 -0500

    changed config to prep for push to heroku

--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
-require('dotenv').config()
+require('dotenv').config();
+const cors = require('cors');
 
-const knex = require('knex')
-const app = require('./app')
-const { PORT, DB_URL } = require('./config')
+const knex = require('knex');
+const app = require('./app');
+const { PORT, DB_URL } = require('./config');
 
 const db = knex({
   client: 'pg',
   connection: DB_URL,
-})
+});
 
-app.set('db', db)
+app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+app.set('db', db);
+
+app.listen(PORT, () => {});
